b7717b11986db95db336b4f211eaf19b4bd3e6cf
igorpardinho <pardinhoigor@gmail.com>
2024-06-20 16:31:36 -0300

    criação dos metodos CRUD no service, e do DTO

---
+++ src/main/java/com/desafiobackendjr/dto/TodoDTO.java
@@ -0,0 +1,4 @@
+package com.desafiobackendjr.dto;
+
+public record TodoDTO(String name, String description, boolean realized, int priority) {
+}
--- src/main/java/com/desafiobackendjr/service/TodoService.java
+++ src/main/java/com/desafiobackendjr/service/TodoService.java
@@ -2,26 +2,44 @@ package com.desafiobackendjr.service;
 
 import com.desafiobackendjr.entity.Todo;
 import com.desafiobackendjr.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TodoService {
 
     private final TodoRepository todoRepository;
 
-    public TodoService(TodoRepository todoRepository){
+    public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
-    public List<Todo> getAllTasks(){
-        return todoRepository.findAll();
+    public List<Todo> getAll() {
+        Sort sort = Sort.by("priority").descending().and(
+                Sort.by("name").ascending()
+        );
+        return todoRepository.findAll(sort);
     }
 
-    public Todo findByName(String name){
-        return todoRepository.findBy(name);
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return getAll();
     }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return getAll();
+    }
+
+    public List<Todo> delete(UUID id) {
+       todoRepository.deleteById(id);
+        return getAll();
+    }
+
 
 
 }
