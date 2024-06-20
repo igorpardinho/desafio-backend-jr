70e18eb6bf9dc1d41d1260af5ceb0919bd6fd909
igorpardinho <pardinhoigor@gmail.com>
2024-06-20 15:32:19 -0300

    first commit

---
+++ src/test/java/com/desafiobackendjr/DesafioBackendJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiobackendjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
