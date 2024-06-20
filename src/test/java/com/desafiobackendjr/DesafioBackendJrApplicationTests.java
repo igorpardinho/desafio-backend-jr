7f57128d897f15ed8e755f88ca727c9bc16a4240
igorpardinho <pardinhoigor@gmail.com>
2024-06-20 15:35:03 -0300

    initial files

--- src/test/java/com/desafiobackendjr/DesafioBackendJrApplicationTests.java
+++ src/test/java/com/desafiobackendjr/DesafioBackendJrApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DesafioBackendJrApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
